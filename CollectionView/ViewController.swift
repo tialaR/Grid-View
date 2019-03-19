906b25c4600f8552a5257290c130c025847902e1
Tiala Rocha <tiala@hotmail.com>
2019-03-19 08:44:03 -0300

    Initial Commit

---
+++ CollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionView
+//
+//  Created by Tiala Rocha on 19/03/2019.
+//  Copyright © 2019 Tiala Rocha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
